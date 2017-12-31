61e96c482003ab6009c3909df561505cdb1bed36
Hoon Lee <yl.hoony@gmail.com>
2017-12-31 12:10:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,7 +36,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)''}" unless send(col_name).nil?
+      values << "'#{send(col_name)'}" unless send(col_name).nil?
     end
     values.join(", ")
   end

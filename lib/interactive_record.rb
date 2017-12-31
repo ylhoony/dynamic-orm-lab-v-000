41966f0c9b0196d95fe98cc08a2aa27de3494dd6
Hoon Lee <yl.hoony@gmail.com>
2017-12-31 11:46:01 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,7 +14,7 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      column_names << row.name
+      column_names << row["name"]
     end
     column_names
   end

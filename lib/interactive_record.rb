1db998f7191cc6b56e70844c36d2eb59bc67b0be
Hoon Lee <yl.hoony@gmail.com>
2017-12-31 12:01:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,7 +29,7 @@ class InteractiveRecord
     self.class.table_name
   end
 
-  def col_name_for_insert
+  def col_names_for_insert
     # self.class.column_names.delete_if {|col_name| col_name == "id"}.join(", ")
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
